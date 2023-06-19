4591868547f299afb621eda1fde592fd362d4cdd
Tantannn <tango.11@yahoo.com>
2023-06-19 17:27:44 +0700

    add route

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
-import { Text, YStack } from "tamagui";
+import { Text, YStack, Button } from "tamagui";
 import React from "react";
-import { useRouter } from "expo-router";
+import { Link, useRouter } from "expo-router";
 
 import { Image } from "react-native";
 
@@ -10,6 +10,12 @@ export default function HomeScreen() {
   return (
     <YStack space="$2" p={"$2"} bg="$gray2" flex={1}>
       <Text>Hello</Text>
+      <Link href="/players">go to me!</Link>
+       <Button
+          onPress={() => router.push("players")}
+        >
+          Capture
+        </Button>
     </YStack>
   );
 }
